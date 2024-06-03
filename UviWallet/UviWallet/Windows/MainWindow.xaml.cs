73d51e82c4fa747ad335b8e54d811a2fbec214d2
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-06-03 23:10:13 +0300

    Updated title bar in main window

--- UviWallet/UviWallet/Windows/MainWindow.xaml.cs
+++ UviWallet/UviWallet/Windows/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 
 namespace UviWallet.Windows {
     public sealed partial class MainWindow : Window {
         public MainWindow() {
             this.InitializeComponent();
+
+            this.AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+            this.AppWindow.TitleBar.BackgroundColor = Colors.Transparent;
+            this.AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            this.AppWindow.TitleBar.InactiveBackgroundColor = Colors.Transparent;
         }
     }
 }
