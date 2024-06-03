aa5c4a76524a65fdc71ebb470a21d2b09bfc0879
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-06-03 23:52:12 +0300

    Updated navigation view Added dashboard, wallets and tokens navigation view items Added SelectionChangeHandler for navigation view Added dashboard, dollar, wallet icons

--- UviWallet/UviWallet/Windows/MainWindow.xaml.cs
+++ UviWallet/UviWallet/Windows/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using UviWallet.Pages;
 
 namespace UviWallet.Windows {
     public sealed partial class MainWindow : Window {
@@ -8,8 +10,25 @@ namespace UviWallet.Windows {
 
             this.AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
             this.AppWindow.TitleBar.BackgroundColor = Colors.Transparent;
-            this.AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            this.AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;  
             this.AppWindow.TitleBar.InactiveBackgroundColor = Colors.Transparent;
+        }
+
+        private void SelectionChangeHandler(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
+            switch (args.SelectedItemContainer.Tag) {
+                case "Dashboard": {
+                    rootFrame.Navigate(typeof(DashboardPage));
+                    break;
+                }
+                case "Wallets": {
+                    rootFrame.Navigate(typeof(WalletsPage));
+                    break;
+                }
+                case "Tokens": {
+                    rootFrame.Navigate(typeof(TokensPage));
+                    break;
+                }
+            }
         }
     }
 }
