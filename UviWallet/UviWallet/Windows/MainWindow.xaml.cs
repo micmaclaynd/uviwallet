b487ea79f4be1e24861b699dbb6408024909946a
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-06-03 23:02:37 +0300

    Updated file struct

---
+++ UviWallet/UviWallet/Windows/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+using Microsoft.UI.Xaml;
+
+namespace UviWallet.Windows {
+    public sealed partial class MainWindow : Window {
+        public MainWindow() {
+            this.InitializeComponent();
+        }
+    }
+}
