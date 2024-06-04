2b28afd42e1b13e22ed607838f109996fc170189
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-06-04 14:45:45 +0300

    Added navigation service

---
+++ UviWallet/UviWallet/Services/NavigationService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using UviWallet.Pages;
+
+namespace UviWallet.Services {
+    public static class NavigationService {
+        public static Frame RootFrame { get; set; }
+        public static NavigationView RootNavigationView { get; set; }
+
+        public static void Navigate(string tag) {
+            foreach (var menuItem in RootNavigationView.MenuItems) {
+                var navigationViewItem = menuItem as NavigationViewItem;
+
+                if (navigationViewItem.Tag.ToString() == tag) {
+                    RootNavigationView.SelectedItem = menuItem;
+
+                    switch (tag) {
+                        case "Dashboard": {
+                            RootFrame.Navigate(typeof(DashboardPage));
+                            break;
+                        } case "Wallets": {
+                            RootFrame.Navigate(typeof(WalletsPage));
+                            break;
+                        } case "Tokens": {
+                            RootFrame.Navigate(typeof(TokensPage));
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}
