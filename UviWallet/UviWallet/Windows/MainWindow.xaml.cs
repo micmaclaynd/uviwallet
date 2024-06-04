14a6ba13498080853e08ec94ee82eb9321dd3824
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-06-04 15:04:26 +0300

    Added use navigation service in main window

--- UviWallet/UviWallet/Windows/MainWindow.xaml.cs
+++ UviWallet/UviWallet/Windows/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using UviWallet.Pages;
+using UviWallet.Services;
 
 namespace UviWallet.Windows {
     public sealed partial class MainWindow : Window {
@@ -10,25 +11,17 @@ namespace UviWallet.Windows {
 
             this.AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
             this.AppWindow.TitleBar.BackgroundColor = Colors.Transparent;
-            this.AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;  
+            this.AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
             this.AppWindow.TitleBar.InactiveBackgroundColor = Colors.Transparent;
+
+            NavigationService.RootFrame = rootFrame;
+            NavigationService.RootNavigationView = rootNavigationView;
+
+            NavigationService.Navigate("Dashboard");
         }
 
         private void SelectionChangeHandler(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
-            switch (args.SelectedItemContainer.Tag) {
-                case "Dashboard": {
-                    rootFrame.Navigate(typeof(DashboardPage));
-                    break;
-                }
-                case "Wallets": {
-                    rootFrame.Navigate(typeof(WalletsPage));
-                    break;
-                }
-                case "Tokens": {
-                    rootFrame.Navigate(typeof(TokensPage));
-                    break;
-                }
-            }
+            NavigationService.Navigate(args.SelectedItemContainer.Tag.ToString());
         }
     }
 }
