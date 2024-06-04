36701ee93ecba8f4a5cff17c831d94f2f2b6f4ad
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-06-04 15:04:35 +0300

    Added settings page

---
+++ UviWallet/UviWallet/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace UviWallet.Pages {
+    public sealed partial class SettingsPage : Page {
+        public SettingsPage() {
+            this.InitializeComponent();
+        }
+    }
+}
